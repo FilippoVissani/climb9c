13b021728c79258fe854fe88fe564791e6da9263
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-16 13:56:24 +0100

    Added search bar as a separate template

--- html/index.php
+++ html/index.php
@@ -1,4 +1,5 @@
 <?php
 $templateParams["title"]="Climb9c - Home";
+$templateParams["search_bar"] = TRUE;
 require 'template/base.php';
 ?>
---
+++ html/template/search-bar.php
@@ -0,0 +1,13 @@
+<div class="row mx-0">
+    <div class="col-12">
+        <form action="#">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control rounded-pill-start" placeholder="Cerca" aria-label="Cerca"
+                    aria-describedby="button-search">
+                <button class="btn bg-primary rounded-pill-bottom" type="button" id="button-search">
+                    <img src="./svg_icons/search.svg" width="30" height="30" class="img-fluid" alt="">
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
