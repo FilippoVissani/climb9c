0dd96a5d481a1d6d07a100116082c61dd3356d0e
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-16 23:14:30 +0100

    SingleProduct data are now collected from the db

--- html/product.php
+++ html/product.php
@@ -1,10 +1,23 @@
 <?php
+require_once 'bootstrap.php';
 
-if(isset($_GET["id"])){
-    $idProduct = $_GET["id"];
+//redirect alla home se non c'è l'id in GET
+if(!isset($_GET["id"])){
+    header("location: index.php");
 }
 
-$templateParams["title"]="Climb9c - ".$idProduct; //fare query per prendere nome articolo
+$idProduct = $_GET["id"];
+
+$templateParams["product"]=$dbh->getProductById($idProduct);
+
+//se non trova l'articolo scrive nel title 'articolo non trovato'
+if(!count($templateParams["product"])==0){
+    $templateParams["product"]=$templateParams["product"][0];
+    $templateParams["title"]="Climb9c - ".$templateParams["product"]["name"];
+} else{
+    $templateParams["title"]="Climb9c - Articolo non trovato";
+}
+
 $templateParams["search_bar"] = TRUE;
 $templateParams["name"]="single_product.php";
 require 'template/base.php';
--- html/template/single_product.php
+++ html/template/single_product.php
@@ -1,21 +1,28 @@
-<div class="container-lg">
-    <div class="row">
-        <div class="col-12">
-            <img src="./upload/products_images_by_id/<?php echo $idProduct; ?>/1.jpg" class="card-img-top" alt="...">
-            <div class="row">
-                <div class="col-9">
-                    <h5>Nike Air Max</h5>
+<?php if(count($templateParams["product"])==0): ?>
+    <div class="alert alert-danger" role="alert">
+        L'articolo non esiste. <a href="index.php" class="alert-link">Clicca qui per andare alla home</a>
+    </div>
+<?php else:
+    $product = $templateParams["product"];
+?>
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-12">
+                <img src="./upload/products_images_by_id/<?php echo $product["idPRODUCT"]; ?>/1.jpg" class="card-img-top" alt="...">
+                <div class="row">
+                    <div class="col-9">
+                        <h5><?php echo $product["name"]; ?></h5>
+                    </div>
+                    <div class="col-3">
+                        <span class="badge bg-success "><?php echo $product["price"]; ?> €</span>
+                    </div>
                 </div>
-                <div class="col-3">
-                    <span class="badge bg-success ">129,99€</span>
+                <div class="row">
+                    <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
                 </div>
-            </div>
-            <div class="row">
-                <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
-            </div>
-                <p class="card-text">La scarpa perfetta per i tuoi piedi. Taglia unisex</p>
+                    <p class="card-text"><?php echo $product["description"]; ?></p>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
+<?php endif; ?>
