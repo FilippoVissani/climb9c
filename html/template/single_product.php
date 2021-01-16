7bb074ebef4a5fea352ed0118cd10e2d17b15737
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-17 00:17:59 +0100

    Product showed in "card" class

--- html/template/single_product.php
+++ html/template/single_product.php
@@ -6,7 +6,32 @@
 <?php else:
     $product = $templateParams["product"];
 ?>
-        <div class="row">
+
+<div class="col-12">
+    <div class="card">
+        <img class="card-img-top" src="./upload/products_images_by_id/<?php echo $product["idPRODUCT"]; ?>/1.jpg" alt="<?php echo $product["name"]; ?>">
+        <div class="card-body">
+            <div class="row mb-2">
+                <div class="col-10"> 
+                    <h5 class="card-title"><?php echo $product["name"]; ?></h5>
+                </div>
+                <div class="col-2">
+                    <span class="badge bg-success "><?php echo $product["price"]; ?> €</span>
+                </div>
+            </div>
+            <a href="#" class="btn btn-primary d-grid">Aggiungi al carrello</a>
+            <p class="card-text"><?php echo $product["description"]; ?></p>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+        <!--
+            <div class="row">
             <div class="col-12">
                 <img src="./upload/products_images_by_id/<?php echo $product["idPRODUCT"]; ?>/1.jpg" class="card-img-top" alt="...">
                 <div class="row">
@@ -24,6 +49,7 @@
                 </div>
             </div>
         </div>
+        -->
     
 <?php endif; ?>
 </div>
