7f653716c7f44a53c2126beabc0381c7d89e485f
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-16 23:12:22 +0100

    Database Helper

---
+++ html/db/database.php
@@ -0,0 +1,22 @@
+<?php
+class DatabaseHelper{
+    private $db;
+
+    public function __construct($servername, $username, $password, $dbname, $port){
+        $this->db = new mysqli($servername, $username, $password, $dbname, $port);
+        if ($this->db->connect_error) {
+            die("Connection failed: " . $db->connect_error);
+        }        
+    }
+
+    public function getProductById($idProduct){
+        $query = "SELECT idPRODUCT, name, price, description FROM PRODUCT WHERE idPRODUCT=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idProduct);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+}
+?>
