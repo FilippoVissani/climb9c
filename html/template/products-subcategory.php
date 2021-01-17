fc8f8dc1bde7519c73f538109a30054f20981bd4
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-17 22:29:01 +0100

    Every product in the list linked to his page

--- html/template/products-subcategory.php
+++ html/template/products-subcategory.php
@@ -9,7 +9,7 @@
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $product["name"]; ?></h5>
                     <p class="card-text text-center"><?php echo $product["price"]; ?> €</p>
-                    <a href="./product.php?id=2" class="stretched-link"></a>
+                    <a href="./product.php?id=<?php echo $product["idPRODUCT"]; ?>" class="stretched-link"></a>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">Disponibili <?php echo $product["quantity"]; ?> pezzi</small>
@@ -18,67 +18,5 @@
             </div>
         <?php endforeach; ?>
 
-
-
-
-
-    <!--
-
-
-    <div class="col">
-        <div class="card h-100">
-        <img src="./upload/products_images_by_id/1/2.jpg" class="card-img-top" alt="<?php echo $templateParams["productsInSubcategory"][1]["name"]; ?>">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo $templateParams["productsInSubcategory"][1]["name"]; ?></h5>
-            <p class="card-text text-center"><?php echo $templateParams["productsInSubcategory"][1]["price"]; ?> €</p>
-            <a href="./product.php?id=2" class="stretched-link"></a>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Disponibili <?php echo $templateParams["productsInSubcategory"][1]["quantity"]; ?> pezzi</small>
-        </div>
-        </div>
     </div>
-
-    <div class="col">
-        <div class="card h-100">
-        <img src="./upload/products_images_by_id/2/1.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Disponibili 3 pezzi</small>
-        </div>
-        </div>
-    </div>
-
-    <div class="col">
-        <div class="card h-100">
-        <img src="./upload/products_images_by_id/2/1.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Disponibili 3 pezzi</small>
-        </div>
-        </div>
-    </div>
-    
-    <div class="col">
-        <div class="card h-100">
-        <img src="./upload/products_images_by_id/2/1.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Disponibili 3 pezzi</small>
-        </div>
-        </div>
-    </div>
-        -->
-
-    </div>
-
 </div>
