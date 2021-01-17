a188740fa1b14bebb65dae2756f628698e817377
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-17 11:10:04 +0100

    Better responsive behaviour

--- html/js/toggle-menu.js
+++ html/js/toggle-menu.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     $("img#menu-toggler").click(function(){
-        $( "div.container-fluid" ).slideToggle( "slow" );
         $( "div#menu" ).slideToggle( "slow" );
+        if ($(window).width() < 1366) {
+            $( "div.container-fluid" ).slideToggle( "slow" );
+        }
     });
+
     $("img#menu-back").click(function(){
-        $( "div.container-fluid" ).slideToggle( "slow" );
         $( "div#menu" ).slideToggle( "slow" );
+        if ($(window).width() < 1366) {
+            $( "div.container-fluid" ).slideToggle( "slow" );
+        }
     });
 });
