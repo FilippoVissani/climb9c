be691bc423ebfcaaa61d61eee0caf815b7ead610
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-17 20:53:54 +0100

    Added functions in database.php

--- html/db/database.php
+++ html/db/database.php
@@ -10,9 +10,29 @@ class DatabaseHelper{
     }
 
     public function getProductById($idProduct){
-        $query = "SELECT idPRODUCT, name, price, description, tecnical_specifications FROM PRODUCT WHERE idPRODUCT=?";
+        $query = "SELECT idPRODUCT, name, price, description, tecnical_specifications FROM product WHERE idPRODUCT=?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('i',$idProduct);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getSubcategoryById($idSubcategory){
+        $query = "SELECT idSUBCATEGORY, idCATEGORY , name FROM subcategory WHERE idSUBCATEGORY=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idSubcategory);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getproductsInSubcategory($idSubcategory){
+        $query = "SELECT p.idPRODUCT, p.name, p.price FROM product as p INNER JOIN subcategory as s ON p.idSUBCATEGORY = s.idSUBCATEGORY WHERE s.idSUBCATEGORY=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idSubcategory);
         $stmt->execute();
         $result = $stmt->get_result();
 
