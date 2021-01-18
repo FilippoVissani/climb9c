786dbbf45e8fdb1f41a835c2cbad8960cdae3c58
Sofia <sofia.belloni@studio.unibo.it>
2021-01-18 19:07:24 +0100

    Almost completed sign up. Added addNewAddressToCustomer(), addNewCustomer(), checkEmailPresence() and getCustomerIdByEmail() functions to database

---
+++ html/sign.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'bootstrap.php';
+
+if(count($_POST) > 0){
+  //check username esistente || check pw != pw-confirm
+  if($dbh->checkEmailPresence($_POST["username"]) == TRUE || $_POST["password"] != $_POST["password-confirm"]){
+    $templateParams["errorelogin"] = "Errore! Dati inseriti non validi";
+    $templateParams["title"]="Climb9c - Registrati";
+    $templateParams["search_bar"] = FALSE;
+    $templateParams["name"]="sign_up2.php";
+  }
+  else{
+    //add customer to db
+    $dbh->addNewCustomer($_POST["username"], $_POST["password"], $_POST["name"], $_POST["surname"], $_POST["telephone"], $_POST["gender"], $_POST["birthdate"]);
+    //inserisci indirizzo nel db
+    $dbh->addNewAddressToCustomer($_POST["username"], $_POST["name"], $_POST["surname"], $_POST["address"], $_POST["province"], $_POST["city"], $_POST["zip_code"]);
+    //visualizza pagina account
+    $templateParams["title"]="Climb9c - Account";
+    $templateParams["search_bar"] = FALSE;
+    $templateParams["name"]="account.php";
+  }
+
+}
+else{
+  $templateParams["title"]="Climb9c - Registrati";
+  $templateParams["search_bar"] = FALSE;
+  $templateParams["name"]="sign_up2.php";
+}
+require 'template/base.php';
+?>
--- html/template/sign_up.php
+++ html/template/sign_up.php
@@ -8,63 +8,64 @@
   </div>
   <hr/>
   <main>
-    <form class="row g-3" action="#" method="post">
+    <form class="row g-3" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <?php if(isset($templateParams["errorelogin"])): ?>
+      <p class="fw-bold  bg-danger text-white"><?php echo $templateParams["errorelogin"]; ?></p>
+      <?php endif; ?>
       <div class="col-md-6  pt-2">
         <label for="username" class="form-label">Email*</label>
-        <input id="username" type="email" class="form-control" value="" required/>
+        <input id="username" name="username" type="email" class="form-control" value="" required/>
       </div>
       <div class="col-md-6  pt-2">
         <label for="password" class="form-label">Password*</label>
-        <input id="password" type="password" class="form-control" value="" required/>
+        <input id="password" name="password" type="password" class="form-control" value="" required/>
       </div>
       <div class="col-md-6  pt-2">
         <label for="password-confirm" class="form-label">Conferma password*</label>
-        <input id="password-confirm" type="password" class="form-control" value="" required/>
+        <input id="password-confirm" name="password-confirm" type="password" class="form-control" value="" required/>
       </div>
       <div class="col-md-6 pt-2">
         <label for="name" class="form-label">Nome*</label>
-        <input id="name" type="text" class="form-control" value="" required/>
+        <input id="name" name="name" type="text" class="form-control" value="" required/>
       </div>
       <div class="col-md-6  pt-2">
         <label for="surname" class="form-label">Cognome*</label>
-        <input id="surname" type="text" class="form-control" value="" required/>
+        <input id="surname" name="surname" type="text" class="form-control" value="" required/>
       </div>
       <div class="col-md-6  pt-2">
         <label for="telephone" class="form-label">Numero di telefono*</label>
-        <input id="telephone" type="tel" class="form-control" value="" required/>
+        <input id="telephone" name="telephone" type="tel" class="form-control" value="" required/>
       </div>
       <fieldset class="col-md-6">
         <legend class="col-form-label  pt-2">Genere</legend>
         <div class="col-md-6">
           <div class="form-check">
-            <input id="gender" type="radio" class="form-check-input"/>
-            <label for="gender" class="form-check-label">Uomo</label>
-          </div>
-          <div class="form-check">
-            <input id="gender" type="radio" class="form-check-input"/>
-            <label for="gender" class="form-check-label">Donna</label>
+            <input type="radio" id="male" name="gender" value="1"/>
+            <label for="male">Uomo</label><br>
+            <input type="radio" id="female" name="gender" value="0"/>
+            <label for="female">Donna</label><br>
           </div>
         </div>
       </fieldset>
       <div class="col-md-6 pt-2">
-        <label for="birthdate" class="form-label">Data di nascita</label>
-        <input id="birthdate" class="form-control" type="date"/>
+        <label for="birthdate" class="form-label">Data di nascita*</label>
+        <input id="birthdate" name="birthdate" class="form-control" type="date" required/>
       </div>
         <div class="col-md-6 pt-2">
           <label for="address" class="form-label">Indirizzo*</label>
-          <input id="address" type="text" class="form-control" value="" required/>
+          <input id="address" name="address" type="text" class="form-control" value="" required/>
         </div>
         <div class="col-md-6  pt-2">
           <label for="city" class="form-label">Città*</label>
-          <input id="city" type="text" class="form-control" value="" required/>
+          <input id="city" name="city" type="text" class="form-control" value="" required/>
         </div>
         <div class="col-md-6  pt-2">
           <label for="province" class="form-label">Provincia*</label>
-          <input id="province" type="text" class="form-control" value="" required/>
+          <input id="province" name="province" type="text" class="form-control" value="" required/>
         </div>
         <div class="col-md-6  pt-2">
           <label for="zip" class="form-label">CAP*</label>
-          <input id="zip" type="text" class="form-control" value="" required/>
+          <input id="zip" name="zip_code" type="text" class="form-control" value="" required/>
         </div>
       <div class="col-md-12 justify-content-center h-100 p-3">
         <label for="registrati" class="invisible">Registrati</label>
