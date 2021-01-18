671bb3ebc6fa88d047a21ad43212c03e10abc4e5
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-18 15:22:22 +0100

    Modifiche sulle insert. DA FINIRE!

--- db/insert.sql
+++ db/insert.sql
@@ -1,24 +1,58 @@
 
 -- insert sulle categorie
 
-INSERT INTO `Climb_9c`.`CATEGORY` (`idCATEGORY`, `name`) VALUES (1, "Scarpe");
+INSERT INTO `climb_9c`.`category` (`idCATEGORY`, `name`) VALUES (1, "Abbigliamento");
 
 -- insert sulle sottocategorie 
 
-INSERT INTO `Climb_9c`.`SUBCATEGORY` (`idSUBCATEGORY`, `idCATEGORY`, `name`) VALUES (1, 1, "Dure");
+INSERT INTO `climb_9c`.`subcategory` (`idSUBCATEGORY`, `idCATEGORY`, `name`) VALUES (1, 1, "Scarpe");
 
-INSERT INTO `Climb_9c`.`SUBCATEGORY` (`idSUBCATEGORY`, `idCATEGORY`, `name`) VALUES (2, 1, "Morbide");
+INSERT INTO `climb_9c`.`subcategory` (`idSUBCATEGORY`, `idCATEGORY`, `name`) VALUES (2, 1, "Caschi");
 
 -- insert sul prodotto
 
--- ID1
-INSERT INTO `Climb_9c`.`PRODUCT` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `image_folder`, `quantity`) VALUES (1, "Nike Air Max", "NIKE", 123, 1, "La descrizione è molto breve: Scarpe molto performanti, adatte a tutti i piedi.", "Qui ci vanno le specifiche tecniche", ".\\upload\\products_images_by_id\\1", 8);
+-- 1
+INSERT INTO `climb_9c`.`product` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `quantity`) VALUES (1, "5.10 Five Ten Aleon VCS scarpette arrampicata", "Five Ten", "99.70", 1, "5.10 Five Ten Aleon VCS sono delle scarpette da arrampicata pensata per il boulder o per l'arrampicata in falesia. Tra tutte le novità da parte di Adidas Five Ten, le Aleon sono sicuramente le scarpette che meritano uno sguardo in più.
 
--- DOPO AVER CARICATO LE NIKE FARE QUESTO UPDATE PER AVERE LE SCARPE DA ARRAMPICATA:
-UPDATE `product` SET `name` = '5.10 Five Ten Aleon VCS scarpette arrampicata', `brand` = 'Five Ten', `description` = '5.10 Five Ten Aleon VCS sono delle scarpette da arrampicata pensata per il boulder o per l\'arrampicata in falesia. Tra tutte le novità da parte di Adidas Five Ten, le Aleon sono sicuramente le scarpette che meritano uno sguardo in più.\r\n\r\nCon la tomaia Ptimeknit sono estremamente avvolgenti e non appena le indossi la loro calzata ti stupirà: completamente fascianti, non lasciano vuoti d\'aria all\'interno e ti sembreranno dei calzini!\r\n\r\nLa punta arcuata verso il basso ti permette di fare degli ottimi agganci grazie anche alla copertura di gomma e il tallone ti facilita soprattutto nel boulder!\r\n\r\nLa suola è una Stealth C4, classica suola presente nelle scarpette Five Ten. Ormai super conosciuta, dona un grip senza precedenti!\r\n\r\nAttenzione! Le scarpette da arrampicata 5.10 Five Ten vestono poco. Prima quindi di procedere all\'acquisto prova a vedere qui sotto i numeri consigliati da noi.', `tecnical_specifications` = 'Marchio\r\nFive Ten\r\nChiusura\r\nStrappo\r\nUtilizzo scarpetta\r\nBoulder, Falesia\r\nTipo suola scarpetta\r\nSuola intera\r\nAggressività scarpetta\r\nPerformante\r\nMateriale tomaia\r\nPrimeknit\r\nMateriale suola\r\nStealth C4\r\nRigidità scarpetta\r\nMedia\r\nCede\r\nPoco\r\nGenere\r\nUnisex\r\nPianta scarpetta arrampicata\r\nLarga\r\nConsigliato rispetto alla taglia normale - Calzata comoda\r\nUn numero in più\r\nConsigliato rispetto alla taglia normale - Calzata media\r\nMezzo numero in più\r\nConsigliato rispetto alla taglia normale - Calzata performante\r\nStesso numero' WHERE `product`.`idPRODUCT` = 1
+Con la tomaia Ptimeknit sono estremamente avvolgenti e non appena le indossi la loro calzata ti stupirà: completamente fascianti, non lasciano vuoti d'aria all'interno e ti sembreranno dei calzini!
 
--- ID2
-INSERT INTO `Climb_9c`.`PRODUCT` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `image_folder`, `quantity`) VALUES (2, "La Sportiva Aragon Woman scarpette arrampicata donna", "La Sportiva", 85.50, 1, "La Sportiva Aragon Woman è la scarpetta da arrampicata perfetta se sei una prinicipiante o se cerchi un modello comodo, da utilizzare principalmente indoor, ma che si comporta bene anche su roccia!
+La punta arcuata verso il basso ti permette di fare degli ottimi agganci grazie anche alla copertura di gomma e il tallone ti facilita soprattutto nel boulder!
+
+La suola è una Stealth C4, classica suola presente nelle scarpette Five Ten. Ormai super conosciuta, dona un grip senza precedenti!
+
+Attenzione! Le scarpette da arrampicata 5.10 Five Ten vestono poco. Prima quindi di procedere all'acquisto prova a vedere qui sotto i numeri consigliati da noi.", "archio
+Five Ten
+Chiusura
+Strappo
+Utilizzo scarpetta
+Boulder, Falesia
+Tipo suola scarpetta
+Suola intera
+Aggressività scarpetta
+Performante
+Materiale tomaia
+PrimeknitM
+Materiale suola
+Stealth C4
+Rigidità scarpetta
+Media
+Cede
+Poco
+Genere
+Unisex
+Pianta scarpetta arrampicata
+Larga
+Consigliato rispetto alla taglia normale - Calzata comoda
+Un numero in più
+Consigliato rispetto alla taglia normale - Calzata media
+Mezzo numero in più
+Consigliato rispetto alla taglia normale - Calzata performante
+Stesso numero", 8);
+
+
+
+-- 2
+INSERT INTO `climb_9c`.`product` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `quantity`) VALUES (2, "La Sportiva Aragon Woman scarpette arrampicata donna", "La Sportiva", 85.50, 1, "La Sportiva Aragon Woman è la scarpetta da arrampicata perfetta se sei una prinicipiante o se cerchi un modello comodo, da utilizzare principalmente indoor, ma che si comporta bene anche su roccia!
 
 La tomaia realizzata in pelle scamosciata è comoda a contatto con la pelle e con l'uso si adatterà perfettamente alla forma del tuo piede. Il sistema di chiusura a doppio velcro è veloce e comodo da utilizzare e ti permette di togliere e mettere le scarpette in un secondo, oltre a trovare in un lampo la regolazione adatta al tuo piede. Con Aragon il confort di calzata è ai massimi livelli!
 
@@ -49,11 +83,11 @@ Mezzo numero in meno
 Consigliato rispetto alla taglia normale - Calzata media
 Un numero in meno
 Consigliato rispetto alla taglia normale - Calzata performante
-Un numero e mezzo in meno", ".\\upload\\products_images_by_id\\2", 3);
+Un numero e mezzo in meno", 3);
 
 
--- ID3
-INSERT INTO `Climb_9c`.`PRODUCT` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `image_folder`, `quantity`) VALUES (3, "5.10 Five Ten Anasazi Lace - The Pink scarpette arrampicata", "Five Ten", 106.95, 1, "5.10 Five Ten Anasazi Lace The Pink è una scarpetta da arrampicata con chiusura a lacci che si adatta alla perfezione per qualsiasi tipo di arrampicata, sia che si parli di palestre indoor sia di arrampicata su roccia.
+-- 3
+INSERT INTO `climb_9c`.`product` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `quantity`) VALUES (3, "5.10 Five Ten Anasazi Lace - The Pink scarpette arrampicata", "Five Ten", 106.95, 1, "5.10 Five Ten Anasazi Lace The Pink è una scarpetta da arrampicata con chiusura a lacci che si adatta alla perfezione per qualsiasi tipo di arrampicata, sia che si parli di palestre indoor sia di arrampicata su roccia.
 
 La Anasazi Lace è una tra le scarpe più riuscite e storiche prodotte da Five Ten. Famosa è la precisione negli appoggi che la contraddistingue. Questo fa in modo che la scarpetta sia ottima sia per l'arrampicata outdoor sia per le palestre.
 
@@ -90,10 +124,10 @@ Un numero in più
 Consigliato rispetto alla taglia normale - Calzata media
 Stesso numero
 Consigliato rispetto alla taglia normale - Calzata performante
-Stesso numero, Mezzo numero in meno", ".\\upload\\products_images_by_id\\3", 5);
+Stesso numero, Mezzo numero in meno", 5);
 
--- ID4
-INSERT INTO `Climb_9c`.`PRODUCT` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `image_folder`, `quantity`) VALUES (4, "5.10 Five Ten Anasazi Pro scarpette arrampicata", "Five Ten", 115.95, 1, "La scarpetta da arrampicata e boulder  Anasazi Pro di 5.10 Five Ten è polivalente e combina sensibilità e potenza per l'arrampicata a 360 gradi.
+-- 4
+INSERT INTO `climb_9c`.`product` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `quantity`) VALUES (4, "5.10 Five Ten Anasazi Pro scarpette arrampicata", "Five Ten", 115.95, 1, "La scarpetta da arrampicata e boulder  Anasazi Pro di 5.10 Five Ten è polivalente e combina sensibilità e potenza per l'arrampicata a 360 gradi.
 
 Anasazi Pro è dotata di tomaia in tessuto sintetico, suola Stealth C4 fino alla punta per favorirti negli agganci e garantirti il massimo grip.
 
@@ -127,10 +161,10 @@ Consigliato rispetto alla taglia normale - Calzata comoda
 Un numero in più
 Consigliato rispetto alla taglia normale - Calzata media
 Mezzo numero in più
-Consigliato rispetto alla taglia normale - Calzata performante", ".\\upload\\products_images_by_id\\4", 7);
+Consigliato rispetto alla taglia normale - Calzata performante", 7);
 
--- ID5
-INSERT INTO `Climb_9c`.`PRODUCT` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `image_folder`, `quantity`) VALUES (5, "BD Black Diamond Momentum Ash scarpette arrampicata", "Black Diamond", 76.55, 1, "BD Black Diamond Momentum scarpette arrampicata sono la scelta giusta per chi è alla ricerca di un modello comodo e confortevole per compiere i primi passi in parete.
+-- 5
+INSERT INTO `climb_9c`.`product` (`idPRODUCT`, `name`, `brand`, `price`, `idSUBCATEGORY`, `description`, `tecnical_specifications`, `quantity`) VALUES (5, "BD Black Diamond Momentum Ash scarpette arrampicata", "Black Diamond", 76.55, 1, "BD Black Diamond Momentum scarpette arrampicata sono la scelta giusta per chi è alla ricerca di un modello comodo e confortevole per compiere i primi passi in parete.
 
 La mescola Neo Regular è bella rigida e durevole, perfetta per la placca e perfetta se hai bisogno di una bestia da soma da sfruttare a lungo!
 
@@ -171,4 +205,4 @@ Un numero in più
 Consigliato rispetto alla taglia normale - Calzata media
 Mezzo numero in più
 Consigliato rispetto alla taglia normale - Calzata performante
-Stesso numero", ".\\upload\\products_images_by_id\\5", 12);
+Stesso numero", 12);
