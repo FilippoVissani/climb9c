c965eff3eda259dbe1895520e8da3dfec77d4ec6
Sofia <sofia.belloni@studio.unibo.it>
2021-01-19 09:15:19 +0100

    Login and account page

--- html/template/login_form.php
+++ html/template/login_form.php
@@ -8,14 +8,17 @@
       <main>
         <div class="row justify-content-center align-items-center h-100">
           <div class="col-md-5">
-            <form action="#" method="post">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+              <?php if(isset($templateParams["login_error"])): ?>
+              <p class="fw-bold  bg-danger text-white"><?php echo $templateParams["login_error"]; ?></p>
+              <?php endif; ?>
               <div class="form-group">
                 <label for="username" class="invisible">Username</label>
-                <input id="username" type="email" class="form-control" autocomplete="on" placeholder="Username" required />
+                <input id="username" name ="username" type="email" class="form-control" autocomplete="on" placeholder="Username" required />
               </div>
               <div class="form-group">
                 <label for="password" class="invisible">Password</label>
-                <input id="password" type="password" class="form-control" autocomplete="on" placeholder="Password" required/>
+                <input id="password" name="password" type="password" class="form-control" autocomplete="on" placeholder="Password" required/>
               </div>
               <div class="row justify-content-center align-items-center h-100">
                 <label for="accedi" class="invisible">Accedi</label>
