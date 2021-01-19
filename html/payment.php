73c0f5e665457d7a22b30c3d2db951ba7cc443e5
Sofia <sofia.belloni@studio.unibo.it>
2021-01-19 22:14:58 +0100

    Started the implementation of paymentent. Added getCoupon() function to datatabase.php

--- html/payment.php
+++ html/payment.php
@@ -1,7 +1,26 @@
 <?php
 require_once 'bootstrap.php';
 
+if(!isUserLoggedIn()){
+  header("location: login.php");
+}
+if(isset($_POST["address"])){
+  $dbh->addNewAddressToCustomer($_SESSION["email"], $_POST["name"], $_POST["surname"], $_POST["address"], $_POST["province"], $_POST["city"], $_POST["zip_code"]);
+  header("location: payment.php");
+}
+if(isset($_GET["couponCode"])){
+  //check validity
+  echo "ciao";
+  $coupon = $dbh->checkCouponValidity($_GET["couponCode"]);
 
+  if(count($coupon)!==0 && $coupon[0]["validity"] ==1 ){
+    $_SESSION["couponCode"]=$_GET["couponCode"];
+    $_SESSION["couponDiscount"]=$coupon[0]["discount"];
+  }else {
+    $templateParams["coupon_error"] = "Errore! Coupon non valido!";
+  }
+  //aggiungi codice sconto all'ordine
+}
 $templateParams["title"]="Climb9c - Pagamento";
 $templateParams["search_bar"] = FALSE;
 $templateParams["name"] = "payment_form.php";
--- html/template/payment_form.php
+++ html/template/payment_form.php
@@ -5,7 +5,7 @@
 </div>
 <hr/>
 <main>
-  <form action="#" method="post">
+  <!--<form action="#" method="post">-->
     <div class="row m-3">
       <div class="col orderd-first">
         <fieldset>
@@ -23,33 +23,50 @@
       <div class="col-md-4 order-md-2">
         <h2>RIEPILOGO CARRELLO</h2>
         <ul class="list-group">
+          <?php if(isset($templateParams["login_error"])): ?>
+            <p class="fw-bold  bg-danger text-white"><?php echo $templateParams["coupon_error"]; ?></p>
+          <?php endif; ?>
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <p>Articoli</p>
             <p><?php echo numberProduct($dbh->getCartByCustomerID($_SESSION['idCUSTOMER'])); ?></p>
           </li>
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <p>Spese di spedizione</p>
-            <p>0.00$</p>
+            <p>0.00€</p>
           </li>
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <div>
               <p>Codice Sconto</p>
-              <small>CODICESCONTO10</small>
+              <small><?php
+                if(isset($_SESSION["couponCode"])){
+                    echo $_SESSION["couponCode"];
+                }
+                else {
+                  echo "NESSUNCODICESCONTOINSERITO";
+                }
+                ?></small>
             </div>
-            <p>0.00$</p>
+            <p><?php
+              if(isset($_SESSION["couponDiscount"])){
+                  echo $_SESSION["couponDiscount"]."€";
+              }
+              else {
+                echo "0.00€";
+              }
+              ?></p>
           </li>
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <p class="font-weight-bold">TOTALE</p>
             <p><?php echo cartPrice($dbh->getCartByCustomerID($_SESSION['idCUSTOMER']))."€"; ?>
           </li>
         </ul>
-        <form class="card p-2" action="#" method="post">
+        <form class="card p-2" action="payment.php" method="get">
           <div class="input-group">
-            <label for="codiceSconto" class="invisible">Codice sconto</label>
-            <input id="codiceSconto" type="text" class="form-control" placeholder="Codice Sconto"/>
+            <label for="coupon" class="invisible">Codice sconto</label>
+            <input id="coupon" name"couponCode" type="text" class="form-control" placeholder="Codice Sconto"/>
             <div class="input-group-append">
               <label for="inserisci" class="invisible">Inserisci</label>
-              <input type="submit" class="btn btn-secondary" value="Inserisci"/>
+              <input id="inserisci" type="submit" class="btn btn-secondary" value="Inserisci"/>
             </div>
           </div>
         </form>
@@ -57,20 +74,55 @@
     </div>
     <div class="row m-3">
       <div class="col">
-        <form action="#" method="post">
-          <fieldset>
-            <legend>INDIRIZZO DI SPEDIZIONE</legend>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" id="flexRadioDefault1" checked/>
-              <label class="form-check-label" for="flexRadioDefault1">Indirizzo di default</label>
-            </div>
-          </fieldset>
-          <div class="m-3">
-            <label for="aggiungiIndirizzo" class="invisible">Aggiungi indirizzo</label>
-            <input id="aggiungiIndirizzo" type="submit" class="btn btn-secondary btn-sm" value="Aggiungi indirizzo"/>
+        <fieldset>
+          <legend>INDIRIZZO DI SPEDIZIONE</legend>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" id="flexRadioDefault1" checked/>
+            <label class="form-check-label" for="flexRadioDefault1">Indirizzo di default</label>
+          </div>
+        </fieldset>
+        <?php if(!isset($_GET["add_address"])): ?>
+        <div class="m-3">
+          <form class="" action="" method="get">
+            <input type="hidden" name="add_address"/>
+            <label for="add_address" class="invisible">Aggiungi indirizzo</label>
+            <input id="add_address" type="submit" class="btn btn-primary" value="Aggiungi indirizzo"/>
+          </form>
+        </div>
+        <?php endif; ?>
+        <?php if(isset($_GET["add_address"])): ?>
+        <form class="row g-3" action="#" method="post">
+          <div class="col-md-6 pt-2">
+            <label for="address" class="form-label">Indirizzo*</label>
+            <input id="address" name="address" type="text" class="form-control" value="" required/>
+          </div>
+          <div class="col-md-6  pt-2">
+            <label for="city" class="form-label">Città*</label>
+            <input id="city" name="city" type="text" class="form-control" value="" required/>
+          </div>
+          <div class="col-md-6  pt-2">
+            <label for="province" class="form-label">Provincia*</label>
+            <input id="province" name="province" type="text" class="form-control" value="" required/>
+          </div>
+          <div class="col-md-6  pt-2">
+            <label for="zip" class="form-label">CAP*</label>
+            <input id="zip" name="zip_code" type="text" class="form-control" value="" required/>
+          </div>
+          <div class="col-md-6 pt-2">
+            <label for="name" class="form-label">Nome sul campanello*</label>
+            <input id="name" name="name" type="text" class="form-control" value="" required/>
+          </div>
+          <div class="col-md-6  pt-2">
+            <label for="surname" class="form-label">Cognome sul campanello*</label>
+            <input id="surname" name="surname" type="text" class="form-control" value="" required/>
+          </div>
+          <div class="col-md-12 justify-content-center h-100 p-3">
+            <label for="aggiungi" class="invisible">Aggiungi</label>
+            <input id="aggiungi" type="submit" class="btn btn-primary btn-block btn-lg" value="Aggiungi"/>
           </div>
         </form>
+        <?php endif; ?>
       </div>
     </div>
-  </form>
+  <!--</form>-->
 </main>
