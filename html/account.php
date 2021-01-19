d9e6a98ce443d807ea5d65c791e879a3ea3251ae
Sofia <sofia.belloni@studio.unibo.it>
2021-01-19 20:03:26 +0100

    Now you can add new addresses to your account

--- html/account.php
+++ html/account.php
@@ -5,9 +5,10 @@ if(isset($_GET["logout"])){
   unset($_SESSION["idCUSTOMER"]);
   header("location: index.php");
 }
-else {
-  header("location: login.php");
+else if(isset($_POST["address"])){
+  $dbh->addNewAddressToCustomer($_SESSION["email"], $_POST["name"], $_POST["surname"], $_POST["address"], $_POST["province"], $_POST["city"], $_POST["zip_code"]);
 }
+header("location: login.php");
 
 require './template/base.php';
 ?>
