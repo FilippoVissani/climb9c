5af52dd1ba088f081d0837dde2aefe3ec5f2f478
Sofia <sofia.belloni@studio.unibo.it>
2021-01-19 12:23:15 +0100

    Started cart implementation

---
+++ html/payment.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'bootstrap.php';
+$templateParams["title"]="Climb9c - Pagamento";
+$templateParams["search_bar"] = FALSE;
+$templateParams["name"] = "payment.php";
+require './template/base.php';
+?>
