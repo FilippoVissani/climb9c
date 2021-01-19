97c4b215476b656bb035896097e67589e9034eda
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-19 13:30:32 +0100

    Fixed return in function

--- html/db/database.php
+++ html/db/database.php
@@ -148,9 +148,6 @@ class DatabaseHelper{
       $stmt = $this->db->prepare($query);
       $stmt->bind_param('s',$json);
       $stmt->execute();
-      $result = $stmt->get_result();
-
-      return $result->fetch_all(MYSQLI_ASSOC);
     }
 
 }
