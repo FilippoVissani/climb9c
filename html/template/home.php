b6dccbbb68fedf19da9d0302bc96b2c8db4e2227
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-21 12:01:14 +0100

    Fixed bug in home

--- html/template/home.php
+++ html/template/home.php
@@ -65,7 +65,6 @@
             </div>
         </div>
     </div>
-</div>
 
 <div class="row m-auto">
     <div class="col-12 m-auto">
@@ -159,15 +158,3 @@
     </div>
     <?php endforeach; ?>
 </div>
-
-
-<h1>Only 4 test:</h1>
-<a href="subcategory.php?id=1">Mostra i prodotti della sottocategoria con id 1</a>
-<br />
-<a href="product.php?id=2">pagina del prodotto con id 2</a>
-<br />
-<a href="product.php?id=99">pagina del prodotto che non esiste</a>
-<br />
-<a href="subcategory.php?id=5">pagina della categoria che non esiste</a>
-<br />
-<a href="admin.php">admin json</a>
