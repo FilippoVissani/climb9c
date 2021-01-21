770b1692432522143ef8bff8bc9332280b86c001
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-21 16:22:21 +0100

    Refactoring filter modal

--- html/template/products-subcategory.php
+++ html/template/products-subcategory.php
@@ -22,52 +22,41 @@
             <div class="modal fade" id="filters" tabindex="-1" aria-labelledby="filters" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
-                    <div class="modal-header" id="modal-header">
-                        <h5 class="modal-title" id="modal-title">Filtri ricerca</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body" id="modal-body">
-                        <ul class="list-group">
-                        <div class="form-check">
-                            <!--<?php foreach($templateParams["tags"] as $tag): ?>
-                                <li class="list-group-item">
-                                    <input class="form-check-input me-1" type="radio" id="<?php echo $tag["chiave"].$tag["valore"];?>" value="" aria-label="...">
-                                    
-                                    <label class="form-check-label" for="<?php echo $tag["chiave"].$tag["valore"];?>">
-                                        <?php echo $tag["chiave"].": ".$tag["valore"]; ?>
 
-                                    
-                                </li>
-                            <?php endforeach; ?>
-                            -->
+                        <!--HEADER-->
+                        <div class="modal-header" id="modal-header">
+                            <h5 class="modal-title" id="modal-title">Filtri ricerca</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
 
+                        <!--BODY-->
+                        <div class="modal-body" id="modal-body">
                             <?php
                             $group = array();
-
                             foreach ( $templateParams["tags"] as $value ) {
                                 $group[$value['chiave']][] = $value;
                             }
-                            ?> 
+                            ?>
 
                             <div class="form-group">
-                            <?php foreach ($group as $key=>$value): ?>
-                                <label for="<?php echo $key; ?>"><?php echo $key; ?></label>
-                                <select class="form-control" id="<?php echo $key; ?>">
-                                <option>Tutti</option>
-                                <?php foreach ($value as $k=>$v): ?>
-                                    <option><?php echo $v["valore"]; ?></option>
+                                <?php foreach ($group as $key=>$value): ?>
+                                    <label for="<?php echo $key; ?>"><?php echo $key; ?></label>
+                                    <select class="form-control" id="<?php echo $key; ?>">
+                                    <option >Tutti</option>
+                                    <?php foreach ($value as $k=>$v): ?>
+                                        <option><?php echo $v["valore"]; ?></option>
+                                    <?php endforeach; ?>
+                                    </select>
+                                    <br/>
                                 <?php endforeach; ?>
-                                </select>
-                            <?php endforeach; ?>
                             </div>
-
                         </div>
-                        </ul>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btnAnnulla">Annulla</button>
-                        <a class="btn btn-primary" href="#" role="button" id="btnApply">Applica</a>
-                    </div>
+
+                        <!--FOOTER-->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btnAnnulla">Annulla</button>
+                            <a class="btn btn-primary" href="#" role="button" id="btnApply">Applica</a>
+                        </div>
                     </div>
                 </div>
             </div>
