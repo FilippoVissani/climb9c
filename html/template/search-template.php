76d60092a129666f6ffa047ada0286a6b00742f4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-21 23:15:14 +0100

    Added product card template to search

--- html/template/search-template.php
+++ html/template/search-template.php
@@ -7,24 +7,13 @@
 <div class="row my-2 mx-5">
         <?php foreach($templateParams["products"] as $product): ?>
             <div class="col">
-            <div class="card h-100">
-            <img src="<?php echo UPLOAD_DIR.$product["idPRODUCT"]; ?>/1.jpg" class="card-img-top" alt="
-            <?php echo $product["name"]; ?>">
-            <div class="card-body">
-                <p class="card-title">
-                    <?php echo $product["name"]; ?>
-                </p>
-                <p class="card-text text-center">
-                    <?php echo $product["price"]; ?> €
-                </p>
-                <a href="./product.php?id=<?php echo $product[" idPRODUCT"]; ?>" class="stretched-link"></a>
+                <?php
+                $templateParams["idPRODUCT"]=$product["idPRODUCT"];
+                $templateParams["productName"]=$product["name"];
+                $templateParams["productPrice"]=$product["price"];
+                $templateParams["quantity"]=$product["quantity"];
+                require "product-card-template.php";
+                ?>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Disponibili
-                    <?php echo $product["quantity"]; ?> pezzi
-                </small>
-            </div>
-        </div>
-        </div>
         <?php endforeach; ?>
 </div>
