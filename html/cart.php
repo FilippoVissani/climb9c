2154a730780c5f56316fb4acc996cae7bab9617d
Sofia <sofia.belloni@studio.unibo.it>
2021-01-22 23:12:11 +0100

    Renamed cart-template page

--- html/cart.php
+++ html/cart.php
@@ -7,7 +7,7 @@ if(isset($_GET["idproduct"])){
 
 $templateParams["title"]="Climb9c - Carrello";
 $templateParams["search_bar"] = FALSE;
-$templateParams["name"] = "cart.php";
+$templateParams["name"] = "cart-template.php";
 $templateParams["categories"]=$dbh->getCategories();
 foreach($templateParams["categories"] as $category){
     $templateParams[$category["id"]."-subcategory"]=$dbh->getSubcategories($category["id"]);
