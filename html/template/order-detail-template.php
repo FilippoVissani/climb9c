1ea9f08353e0aa55141ff8e463eeb8e5ced60508
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-22 11:24:54 +0100

    Corrected null variable

--- html/template/order-detail-template.php
+++ html/template/order-detail-template.php
@@ -4,28 +4,30 @@
     </div>
 </div>
 
-<?php foreach($templateParams["products"] as $product): ?>
-<div class="row mx-auto mt-3 border-bottom border-3 border-dark">
-    <div class="col-md-3 m-auto">
-        <img class="w-100 shadow mb-5 w-100 px-auto rounded-pill-home" src="<?php echo UPLOAD_DIR.$product["idPRODUCT"]; ?>/1.jpg" />
-    </div>
-    <div class="col-md-9 m-auto">
-        <h1>
-            <?php echo $product["name"] ?>
-        </h1>
-        <p class="fs-5">
-            Brand: <?php echo $product["brand"] ?>
-        </p>
-        <p class="fs-5">
-            Quantità: <?php echo $product["quantity"] ?>
-        </p>
-        <p class="fs-5">
-            Prezzo: <?php echo $product["unit_price"] ?> €
-        </p>
-    </div>
-</div>
 <?php
-$total+=$product["quantity"]*$product["unit_price"];
+$total = 0;
+foreach ($templateParams["products"] as $product) : ?>
+    <div class="row mx-auto mt-3 border-bottom border-3 border-dark">
+        <div class="col-md-3 m-auto">
+            <img class="w-100 shadow mb-5 w-100 px-auto rounded-pill-home" src="<?php echo UPLOAD_DIR . $product["idPRODUCT"]; ?>/1.jpg" />
+        </div>
+        <div class="col-md-9 m-auto">
+            <h1>
+                <?php echo $product["name"] ?>
+            </h1>
+            <p class="fs-5">
+                Brand: <?php echo $product["brand"] ?>
+            </p>
+            <p class="fs-5">
+                Quantità: <?php echo $product["quantity"] ?>
+            </p>
+            <p class="fs-5">
+                Prezzo: <?php echo $product["unit_price"] ?> €
+            </p>
+        </div>
+    </div>
+<?php
+    $total += $product["quantity"] * $product["unit_price"];
 endforeach;
 ?>
 
