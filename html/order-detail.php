9e6869448c45b4ec74db5467357efc360d9384d0
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-22 11:34:43 +0100

    Better style and implementation

--- html/order-detail.php
+++ html/order-detail.php
@@ -1,20 +1,22 @@
 <?php
 require_once 'bootstrap.php';
-$templateParams["title"]="Climb9c - Order detail";
-$templateParams["search_bar"] = FALSE;
-$templateParams["name"] = "order-detail-template.php";
-$templateParams["products"]=$dbh->getProducts($_POST["id"]);
 if(isset($_SESSION["idCUSTOMER"])){
+    $templateParams["title"]="Climb9c - Order detail";
+    $templateParams["search_bar"] = FALSE;
+    $templateParams["name"] = "order-detail-template.php";
+    $templateParams["products"]=$dbh->getProducts($_POST["id"]);
     $customerOrders = $dbh->getOrders($_SESSION["idCUSTOMER"]);
     foreach($customerOrders as $tmp){
         if($tmp["idORDER"]==$_POST["id"]){
             $templateParams["orderDetails"]=$tmp;
         }
     }
+    $templateParams["categories"]=$dbh->getCategories();
+    foreach($templateParams["categories"] as $category){
+        $templateParams[$category["id"]."-subcategory"]=$dbh->getSubcategories($category["id"]);
+    }
+    require './template/base.php';
+}else{
+    header("location: login.php");
 }
-$templateParams["categories"]=$dbh->getCategories();
-foreach($templateParams["categories"] as $category){
-    $templateParams[$category["id"]."-subcategory"]=$dbh->getSubcategories($category["id"]);
-}
-require './template/base.php';
 ?>
--- html/template/menu.php
+++ html/template/menu.php
@@ -6,12 +6,12 @@
     <?php foreach($templateParams["categories"] as $category): ?>
     <ul class="list-group collapse" id="<?php echo $category["id"] ?>-subcategory">
         <?php foreach($templateParams[$category["id"]."-subcategory"] as $subcategory): ?>
-        <li class="list-group-item menu-item">
-            <a class="text-dark fw-bold" href="subcategory.php?id=<?php echo $subcategory["id"] ?>">
-                <?php echo strtoupper($subcategory["name"]) ?>
-                <i class="fas fa-chevron-right fs-3 d-inline-block float-end"></i>
+            <a class="text-dark fw-bold menu-item" href="subcategory.php?id=<?php echo $subcategory["id"] ?>">
+                <li class="list-group-item">
+                    <?php echo strtoupper($subcategory["name"]) ?>
+                    <i class="fas fa-chevron-right fs-3 d-inline-block float-end"></i>
+                </li>
             </a>
-        </li>
         <?php endforeach; ?>
     </ul>
     <?php endforeach; ?>
