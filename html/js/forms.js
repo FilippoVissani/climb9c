72d355dc9a4ba6f68bdfc89c1ef1397ad00791fe
Sofia <sofia.belloni@studio.unibo.it>
2021-01-23 14:10:33 +0100

    Completed password hashing

--- html/js/forms.js
+++ html/js/forms.js
@@ -13,32 +13,30 @@ $(function () {
       form.append(p);
       p.name = "p";
       p.type = "hidden"
-      //p.value= hex_sha512(password.val());
-      p.value = password.val();
+      p.value= sha512(password.val());
+      //p.value = password.val();
       password.val("");
 
       form.append(pc);
       pc.name = "pc";
       pc.type = "hidden"
-      //pc.value= hex_sha512(passwordConfirm.val());
-      pc.value = passwordConfirm.val();
+      pc.value= sha512(passwordConfirm.val());
+      //pc.value = passwordConfirm.val();
       passwordConfirm.val("");
       //submit del form.
       form.submit();
     });
 
     $("#accedi").click(function () {
-      console.log("ciao");
       // Crea un elemento di input che verrà usato come campo di output per la password criptata.
       let p = document.createElement("input");
 
       form.append(p);
       p.name = "p";
       p.type = "hidden"
-      //p.value= hex_sha512(password.val());
-      p.value = password.val();
+      p.value= sha512(password.val());
+      //p.value = password.val();
       password.val("");
-      console.log(p.value);
       //submit del form.
       form.submit();
     });
