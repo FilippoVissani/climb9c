ea03eb54c1787af7a8bb8ddba6bc0ec11d122ea8
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-24 12:14:17 +0100

    Fixed best seller elements size

--- html/template/home-template.php
+++ html/template/home-template.php
@@ -138,7 +138,7 @@
 
 <div class="row m-auto row row-cols-2 row-cols-md-4 g-4">
     <?php foreach($templateParams["bestSeller"] as $product): ?>
-    <div class="col-md-2 mx-auto">
+    <div class="col-md-2 mx-auto d-flex">
         <?php 
         $templateParams["idPRODUCT"]=$product["idPRODUCT"];
         $templateParams["productName"]=$product["name"];
