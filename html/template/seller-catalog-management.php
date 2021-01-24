93122c2f996b7e15e8426d2cab23e06c31c0943a
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-24 19:36:53 +0100

    edited catalog management page

--- html/template/seller-catalog-management.php
+++ html/template/seller-catalog-management.php
@@ -1,6 +1,6 @@
 <div class="row mt-5 mx-2">
-    <div class="col-md-4">
-        <div class="card">
+    <div class="col-md-3">
+        <div class="card mb-3">
             <div class="card-header fw-bold fs-4">
                 Aggiungi prodotto
             </div>
@@ -10,25 +10,36 @@
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="card">
+    <div class="col-md-3">
+        <div class="card mb-3">
             <div class="card-header fw-bold fs-4">
                 Modifica prodotto esistente
             </div>
             <div class="card-body">
                 <p class="card-text fs-4">Entra nell'area dedicata.</p>
-                <a href="" class="btn btn-primary">Entra</a>
+                <a href="#" class="btn btn-primary">Entra</a>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="card">
+    <div class="col-md-3">
+        <div class="card mb-3">
             <div class="card-header fw-bold fs-4">
-                Gestione categorie, sottocategorie e tag
+                Categorie, sottocategorie e tag
             </div>
             <div class="card-body">
                 <p class="card-text fs-4">Entra nell'area dedicata.</p>
                 <a href="seller-manage-categories.php" class="btn btn-primary">Entra</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card mb-3">
+            <div class="card-header fw-bold fs-4">
+                Gestione coupon
+            </div>
+            <div class="card-body">
+                <p class="card-text fs-4">Entra nell'area dedicata.</p>
+                <a href="#" class="btn btn-primary">Entra</a>
             </div>
         </div>
     </div>
