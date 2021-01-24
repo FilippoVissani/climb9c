3f7f2690cff08444760b15c8f50aa66710e53106
Filippo Vissani <filippo.vissani@studio.unibo.it>
2021-01-24 14:32:29 +0100

    Added logout button for seller

--- html/template/seller-home-template.php
+++ html/template/seller-home-template.php
@@ -1,3 +1,9 @@
+<?php
+if($_POST["logout"]==TRUE){
+    unset($_SESSION["idSELLER"]);
+    header("location: login-admin.php");
+}
+?>
 <div class="row mt-5 mx-2">
     <div class="col-md-6">
         <div class="card">
@@ -24,6 +30,9 @@
 </div>
 <div class="row mt-5 mx-2">
     <div class="col-md-12">
-        <button class="btn btn-primary">LOGOUT</button>
+        <form action="./seller-home.php" method="post">
+            <input class="collapse" type="text" name="logout" value="TRUE" readonly="readonly">
+            <button type="submit" class="btn btn-primary fw-bold">Logout</button>
+        </form>
     </div>
 </div>
