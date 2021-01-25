213090c6a07fc114bf1b017aaac034579ac0f468
Eddie Barzi <eddie.barzi@studio.unibo.it>
2021-01-25 18:43:28 +0100

    d-flex card

--- html/template/seller-catalog-management.php
+++ html/template/seller-catalog-management.php
@@ -1,6 +1,6 @@
 <div class="row mt-5 mx-2">
-    <div class="col-md-3">
-        <div class="card mb-3">
+    <div class="col-md-3  d-flex">
+        <div class="card mb-3 flex-fill">
             <div class="card-header fw-bold fs-4">
                 Aggiungi prodotto
             </div>
@@ -10,19 +10,19 @@
             </div>
         </div>
     </div>
-    <div class="col-md-3">
-        <div class="card mb-3">
+    <div class="col-md-3  d-flex">
+        <div class="card mb-3 flex-fill">
             <div class="card-header fw-bold fs-4">
                 Modifica prodotto esistente
             </div>
             <div class="card-body">
                 <p class="card-text fs-4">Entra nell'area dedicata.</p>
-                <a href="#" class="btn btn-primary">Entra</a>
+                <a href="edit-product.php" class="btn btn-primary">Entra</a>
             </div>
         </div>
     </div>
-    <div class="col-md-3">
-        <div class="card mb-3">
+    <div class="col-md-3  d-flex">
+        <div class="card mb-3 flex-fill">
             <div class="card-header fw-bold fs-4">
                 Categorie, sottocategorie e tag
             </div>
@@ -32,8 +32,8 @@
             </div>
         </div>
     </div>
-    <div class="col-md-3">
-        <div class="card mb-3">
+    <div class="col-md-3  d-flex">
+        <div class="card mb-3 flex-fill">
             <div class="card-header fw-bold fs-4">
                 Gestione coupon
             </div>
